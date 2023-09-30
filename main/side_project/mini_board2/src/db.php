9271176353e2a13f414af5ae6ae87bbb10041eae
HongDawww <d939305@naver.com>
2023-09-30 12:17:34 +0900

    0930 commit

--- main/side_project/mini_board2/src/db.php
+++ main/side_project/mini_board2/src/db.php
@@ -1,55 +1,248 @@
 <?php
 
-// 1. my_db_conn
-// db 접속 
-function my_db_conn( &$conn ){
-	$db_host		= "localhost";
-	$db_user		= "root";
-	$db_pw		 	= "php504";
-	$db_name		= "mini_board"; 
-	$db_charset		= "utf8mb4";
-	$db_dns			= "mysql:host=".$db_host.";dbname=".$db_name.";charset=".$db_charset;
-	
-	try{
-		$db_options = [
-			PDO::ATTR_EMULATE_PREPARES 		=> false
-			,PDO::ATTR_ERRMODE 				=> PDO::ERRMODE_EXCEPTION
-			,PDO::ATTR_DEFAULT_FETCH_MODE 	=> PDO::FETCH_ASSOC
-		];
-	
-		$conn =new PDO($db_dns,$db_user,$db_pw,$db_options);
-		return true;
-	} catch (Exception $e) {
+	// ----------------------------------------------
+		// 파일명 : 04_107_fnc_db_conn.php
+		// 기능 : 디비 연동 관련 함수
+		// 버전 : v001 new Hong.dw 230918
+		//        
+	// ----------------------------------------------
+		// 함수명 my_db_conn
+		// 기능 : DB Connect
+		// 파라미터 : PDO &$conn 
+		// 리턴 : boolean
+	// -----------------------------------------------
+
+	function my_db_conn( &$conn ){
+		$db_host		= "localhost";
+		$db_user		= "root";
+		$db_pw		 	= "php504";
+		$db_name		= "mini_board"; 
+		$db_charset		= "utf8mb4";
+		$db_dns			= "mysql:host=".$db_host.";dbname=".$db_name.";charset=".$db_charset;
+		
+		try{
+			$db_options = [
+				PDO::ATTR_EMULATE_PREPARES 		=> false
+				,PDO::ATTR_ERRMODE 				=> PDO::ERRMODE_EXCEPTION
+				,PDO::ATTR_DEFAULT_FETCH_MODE 	=> PDO::FETCH_ASSOC
+			];
+		
+			$conn =new PDO($db_dns,$db_user,$db_pw,$db_options);
+			return true;
+		} catch (Exception $e) {
+			$conn = null;
+			return false;
+		}
+	}
+
+	// ----------------------------------------------
+		// 함수명 db_destroy_conn
+		// 기능 : DB destroy
+		// 파라미터 : PDO &$conn 
+		// 리턴 : 없음
+	// -----------------------------------------------
+	function db_destroy_conn(&$conn) {
 		$conn = null;
-		return false;
 	}
-}
-// 2. db_destroy_conn
-// db = null 
-function db_destroy_conn(&$conn){
-	$conn = null;
-}
 
 
-// 
-function db_select_boards_paging(&$conn,&$arr_param){
-	try{
+	// ----------------------------------------------
+		// 함수명 db_select_boards_paging
+		// 기능 : boards paging 조회
+		// 파라미터 : PDO &$conn 
+		//			Array &$arr_param (쿼리 작성용 배열)
+		// 리턴 : Array / false
+	// -----------------------------------------------
+	function db_select_boards_paging(&$conn ,&$arr_param){
+		try {	
+			$sql =
+			" SELECT "
+			." 		id "
+			." 		,title "
+			." 		,create_at "
+			." 	FROM "
+			." 		boards "
+			." WHERE "
+			." delete_flg = '0' "
+			."	ORDER BY "
+			."		id DESC "
+			." LIMIT :list_cnt OFFSET :offset"
+			;
 
+			$arr_ps = [
+				":list_cnt" => $arr_param["list_cnt"]
+				,":offset" => $arr_param["offset"]
+			];
+
+			$stmt = $conn->prepare($sql);
+			$stmt->execute($arr_ps);
+			$result = $stmt->fetchAll();
+			return $result;
+		} catch(Exception $e){
+			return false;
+		}
+
+	}
+
+	// ----------------------------------------------
+		// 함수명 db_select_boards_cnt
+		// 기능 : boards count 조회
+		// 파라미터 : pdo &$conn
+		//			
+		// 리턴 : int / false
+	// -----------------------------------------------
+	function db_select_boards_cnt(&$conn){
+		try {
+			$sql =
+				" SELECT "
+				." COUNT(id) AS cnt "
+				." FROM "
+				." boards "
+				." WHERE "
+				." delete_flg = '0' "
+				;			
+			$stmt = $conn->query($sql);
+			$result = $stmt->fetchAll();
+
+			return (int)$result[0]["cnt"]; // 정상 : 쿼리 결과 리턴
+
+		} catch (Exception $e){
+			return false; // 예외발생 : false 리턴
+		}
+	}
+	// ----------------------------------------------
+		// 함수명 db_insert_boards
+		// 기능 : boards record 작성
+		// 파라미터 : pdo &$conn
+		//			array &$arr_param 쿼리 작성용 배열
+		// 리턴 :boolean
+	// -----------------------------------------------
+	function db_insert_boards(&$conn, &$arr_param){ 
+		try {
+			$sql =
+			" INSERT INTO boards ( "
+			." title "
+			." ,content "
+			." ) "
+			." VALUES ( "
+			." :title "
+			."  ,:content "
+			." ) "
+			;
+
+			$arr_ps=[
+				":title" => $arr_param["title"]
+				,":content" => $arr_param["content"]
+			];
+
+		
+			$stmt = $conn ->prepare($sql);
+			$result = $stmt->execute($arr_ps);
+			return $result; //결과 리턴
+		} catch (Exception $e) {
+			echo $e->getMessage();
+			return false; //예외발생 false 리턴
+		} 
+	}
+
+	// ----------------------------------------------
+	// 함수명 db_select_boards_id
+	// 기능 :
+	// 파라미터 : pdo &$conn
+	//			array &$arr_param 쿼리 작성용 배열
+	// 리턴 :boolean
+	// -----------------------------------------------
+	function db_select_boards_id(&$conn,&$arr_param){
+		try {	
+			$sql =
+			" SELECT "
+			." id "
+			." ,title "
+			." ,content "
+			." ,create_at "
+			." FROM "
+			." boards"
+			." WHERE "
+			." id = :id "
+			." AND "
+			." delete_flg = '0' "
+			;
+			$arr_ps = [
+				":id" => $arr_param["id"]
+			];
+			
+			$stmt = $conn->prepare($sql);
+			$stmt->execute($arr_ps);
+			$result = $stmt->fetchAll();
+			return $result;
+		} catch(Exception $e){
+			echo $e->getMessage();
+			return false;
+		}
+
+	}
+
+	// ----------------------------------------------
+		// 함수명 db_update_boards_id
+		// 기능 : board 수정
+		// 파라미터 : pdo &$conn
+		//			&$arr_param / 쿼리 작성 배열
+		// 리턴 : boolean
+	// -----------------------------------------------
+	function db_update_boards_id(&$conn, &$arr_param){
 		$sql = 
-		" SELECT "
-		." id"
-		." ,title "
-		." ,create_at "
-		." FROM "
-		." boards "
-		." ORDER BY "
-		." id DESC ";
-		$arr_ps = [
-
+		" UPDATE "
+		." 		boards "
+		."	SET "
+		." 		title = :title "
+		."		,content = :content "
+		." 	WHERE "
+		." 		id = :id ";
+		$arr_ps =
+		[
+			":title" => $arr_param["title"]
+			,":content" => $arr_param["content"]
+			,":id" =>$arr_param["id"]
 		];
-	}catch(Exception $e){
-		return false;
+		try{
+			$stmt = $conn->prepare($sql);
+			$result = $stmt->execute($arr_ps);
+			return $result;
+		} catch(Exception $e) {
+			echo $e->getMessage();
+			return false;
+		}
 	}
-}
 
-?>
+	// ----------------------------------------------
+		// 함수명 db_delete_boards_id
+		// 기능 : 특정 id의 레코드 삭제 처리
+		// 파라미터 : pdo &$conn
+		//			&$arr_param 
+		// 리턴 : boolean
+	// -----------------------------------------------
+	function db_delete_boards_id(&$conn,&$arr_param){
+		$sql = 
+		" UPDATE boards "
+		." SET "
+		." delete_at = now() "
+		." ,delete_flg = '1' "
+		." WHERE "
+		." id = :id "
+		;
+
+		$arr_ps = [
+			":id" => $arr_param["id"]
+		];
+
+		try {
+			//쿼리 실행
+			$stmt = $conn->prepare($sql);
+			$result = $stmt->execute($arr_ps);
+
+			return $result; //정상종료- true 리턴
+		} catch(Exception $e) {
+			echo $e->getMessage();
+			return false;
+		}
+	}
--- main/side_project/mini_board2/src/list.php
+++ main/side_project/mini_board2/src/list.php
@@ -3,6 +3,56 @@ define("ROOT",$_SERVER["DOCUMENT_ROOT"]."/mini_board2/src/");
 define("FILE_HEADER",ROOT."header2.php");
 require_once(ROOT."db.php"); 
 
+$conn = null;
+$list_cnt = 5;
+$page_num = 1;
+
+try {
+	// db 접속
+ if(!(my_db_conn($conn))){
+	throw new Exception("DB Error : PDO Instance");
+ }
+ //총 페이지 카운트
+$boards_cnt = db_select_boards_cnt($conn);
+if($boards_cnt === false){
+	throw new Exception("DB Error : SELECT count");
+}
+//총 페이지 나누기 / 5 = 최대 페이지
+$max_page_num = ceil($boards_cnt / $list_cnt);
+
+if(isset($_GET["page"])){
+	$page_num = $_GET["page"]; //기본페이지
+}
+$offset = ($page_num-1) * $list_cnt; //1페이지마다 보이는 페이지
+$prev_page_num = $page_num -1;
+// 이전버튼 0이면 1
+if($prev_page_num === 0){
+	$prev_page_num = 1;
+}
+//다음버튼 
+//다음버튼을 누르면 +1 되고 최대페이지까지만.
+$next_page_num = $page_num +1;
+if($next_page_num === $max_page_num) {
+	$next_page_num = $max_page_num;
+}
+//변수에 값지정
+$arr_param = [
+"list_cnt" => $list_cnt
+,"offset" => $offset
+];
+
+$result = db_select_boards_paging($conn, $arr_param);
+// 페이지에 보이는 리스트
+if(!$result){
+	throw new Exception("DB Error : SELECT boards");
+}
+
+} catch(Exception $e){
+	echo $e->getMessage();
+	exit;
+} finally{
+	db_destroy_conn($conn);
+}
 
 
 ?>
@@ -41,44 +91,28 @@ require_once(ROOT."db.php");
 			?>
 			<tr>
 				<td><?php echo $item["id"]?></td>
-				<td><?php echo $item["title"]?></td>
+				<td>
+					<a href="/mini_board/src/detail.php/?id=<?php echo $item["id"];?>&page=<?php echo $page_num;?>">
+						<?php echo $item["title"]; ?>
+					</a>
 				<td><?php echo $item["create_at"]?></td>
 			</tr>
 
 			<?php  } ?>
-			<!-- <tr>
-				<td>2</td>
-				<td>22222222222</td>
-				<td>2023-09-25</td>
-			</tr>
-			<tr>
-				<td>3</td>
-				<td>3333333333</td>
-				<td>2023-09-25</td>
-			</tr>
-			<tr>
-				<td>4</td>
-				<td>4444444444</td>
-				<td>2023-09-25</td>
-			</tr>
-			<tr>
-				<td>5</td>
-				<td>5555555555</td>
-				<td>2023-09-25</td>
-			</tr>
-			<tr>
-				<td>6</td>
-				<td>6666666666</td>
-				<td>2023-09-25</td>
-			</tr> -->
-
+			
 		</table>
 		<button type="button"  id="form_btn" ><a href="/mini_board/src/insert2.php">Write</a></button>
 	</form>
 	<section>
-		<a href="/mini_board2/src/list.php/?page=">이전</a>
-		<a href="/mini_board2/src/list.php/?page="></a>
-		<a href="/mini_board2/src/list.php/?page=">다음</a>
+		<a href="/mini_board2/src/list.php/?page=<?php echo $prev_page_num ?>">이전</a>
+		<?php
+			for($i = 1; $i <= $max_page_num; $i++) {
+		?>
+		<a href="/mini_board2/src/list.php/?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+		<?php
+		}
+		?>
+		<a href="/mini_board2/src/list.php/?page=<?php echo $next_page_num ?>">다음</a>
 	</section>
 </body>
 </html>
