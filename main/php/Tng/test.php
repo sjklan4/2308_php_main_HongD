0a7e414cfa99041163ce7c7d8927970c80125fd5
HongDawww <d939305@naver.com>
2023-10-04 11:11:33 +0900

    1004 commit

---
+++ main/php/Tng/test.php
@@ -0,0 +1,20 @@
+<?php
+
+
+define();
+class Test {
+	public static $tt;
+	// public const TT = "a";
+
+}
+
+// Test::TT;
+// echo Test::TT;
+
+// Test::$tt = "b";
+// echo Test::$tt;
+
+// $obj = new Test();
+// echo "1 :".$obj->tt."\n";
+// $obj->tt = "a";
+// echo "2 : ".$obj->tt;
